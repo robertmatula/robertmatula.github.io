0b465cfb554aa7941bb1f872374679634bf13dc7
robertmatula <robertmatula10@gmail.com>
2017-06-29 19:58:50 +0200

    Added overlay menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 require('../scss/main.scss');
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const openMenuButton = document.querySelector('.open-menu');
+    const overlay = document.querySelector('.overlay');
+    const closeButton = document.querySelector('.close-menu');
+
+    openMenuButton.addEventListener('click', event => {
+      overlay.classList.toggle("open");
+      openMenuButton.style.visibility = 'hidden';
+    })
+
+    closeButton.addEventListener('click', event => {
+      overlay.classList.toggle("open");
+      openMenuButton.style.visibility = 'visible';
+    })
+
+});
