bf78ed791ccb7dc718d3a5db01616f300d8c9a0e
robertmatula <robertmatula10@gmail.com>
2017-06-29 14:36:04 +0200

    Added url-loader module

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,11 @@ module.exports = {
       {
         test: /\.scss$/,
         loaders: ["style-loader", "css-loader", "sass-loader"]
-      }
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
+        }
     ]
   }
 }
